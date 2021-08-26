d526a3a71d35421c812f382abf3924f9fe5af0ca
Richard <rdesilvey@gmail.com>
2021-08-26 14:12:56 -0600

    dup code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,11 +5,6 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
-# Add additional requires below this line. Rails is not loaded until this point!
-require File.expand_path('../config/environment', __dir__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'factory_bot'
 require 'vcr'
