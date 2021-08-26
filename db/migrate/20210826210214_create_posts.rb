3ed20850d1ec980a4b058eca301175957f937dec
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-26 15:02:56 -0600

    Generate Post model with relationship tests

---
+++ db/migrate/20210826210214_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.string :image
+      t.integer :upvotes
+      t.integer :downvotes
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
