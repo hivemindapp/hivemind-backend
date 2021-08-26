ed21b7b7cc18987085b5edf5c5aec6e749ee83c8
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-26 15:05:41 -0600

    Generate Comment model with relationship tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
+RSpec.describe Comment, type: :model do
   describe 'relationships' do
     it { should belong_to :user }
-    it { should have_many(:comments).dependent(:destroy) }
+    it { should belong_to :post }
   end
 end
