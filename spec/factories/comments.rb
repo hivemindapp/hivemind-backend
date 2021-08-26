af97cad17cf99cf4bb11d9f7c594e5aa092e0894
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-26 15:45:24 -0600

    Add Faker suggestions to factories

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :comment do
-    content { "MyString" }
-    upvotes { 1 }
-    downvotes { 1 }
-    user { nil }
-    post { nil }
+    content { Faker::Books::Dune.quote }
+    upvotes { Faker::Number.between(from: 1, to: 10) }
+    downvotes { Faker::Number.between(from: 1, to: 10) }
+    user
+    post
   end
 end
