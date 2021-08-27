a2b8c126015908c9fae4fb95b575a9fd5b86e52e
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-27 15:52:57 -0600

    Create first GraphQL test for all posts endpoint

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,7 +4,7 @@ module Types
     field :username, String, null: true
     field :region, String, null: true
     field :biography, String, null: true
-    field :image, String, null: true
+    field :avatar, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
---
+++ spec/graphql/types/query_type_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe '.resolve' do
+    it 'lists all posts' do
+      user1 = create(:user)
+      user2 = create(:user)
+
+      posts1 = create_list(:post, 5, user: user1)
+      posts2 = create_list(:post, 5, user: user2)
+
+      post '/graphql', params: { query: query }
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      data = json[:data][:posts]
+      
+      expect(data).to be_an Array
+      expect(data.length).to eq 10
+
+      expect(data.first[:id]).to eq "1"
+      expect(data.first[:title]).to be_a String
+      expect(data.first[:image]).to be_a String
+      expect(data.first[:upvotes]).to be_a Integer
+      expect(data.first[:downvotes]).to be_a Integer
+      expect(data.first[:createdAt]).to be_a String
+      expect(data.first[:user]).to eq({ id: user1.id.to_s, username: user1.username, avatar: user1.avatar })
+    end
+
+    def query
+      <<~GQL
+        query {
+          posts {
+            id
+            title
+            image
+            upvotes
+            downvotes
+            createdAt
+            user {
+              id
+              username
+              avatar
+            }
+          }
+        }
+      GQL
+    end
+  end
+end
