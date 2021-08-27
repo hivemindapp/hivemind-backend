5978d800706bf9858b0f94dfce4b74ccd51d3bc1
Richard <rdesilvey@gmail.com>
2021-08-26 19:27:20 -0600

    test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'factory_bot'
 require 'vcr'
-
+  
 Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
