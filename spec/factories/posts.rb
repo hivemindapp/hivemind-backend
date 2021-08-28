84577d6a67bdfaa07a343ccf6213df9bd25def14
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-27 20:12:40 -0600

    Update factories to produce randomly sized images

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :post do
     title { Faker::Books::Lovecraft.sentence }
     description { Faker::Books::Lovecraft.paragraph }
-    image { Faker::LoremFlickr.image }
+    image { Faker::LoremFlickr.image(size: "#{rand(200..600)}x#{rand(200..600)}", search_terms: ['bees', 'beehive']) }
     upvotes { Faker::Number.between(from: 1, to: 10) }
     downvotes { Faker::Number.between(from: 1, to: 10) }
     user
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     username { Faker::Fantasy::Tolkien.character }
     region { Faker::Fantasy::Tolkien.location }
     biography { Faker::Fantasy::Tolkien.poem }
-    avatar { Faker::LoremFlickr.image }
+    avatar { Faker::LoremFlickr.image(size: "#{rand(100..250)}x#{rand(100..250)}", search_terms: ['bear']) }
   end
 end
