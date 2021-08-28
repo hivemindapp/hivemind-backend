6a087a18e4482f2f1e0689d3c0ab727aa315dd6c
JoePeterson51 <>
2021-08-28 17:40:30 -0600

    Implement create comment mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,11 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_post, mutation: Mutations::Posts::CreatePost
+<<<<<<< HEAD
     field :create_comment, mutation: Mutations::Comments::CreateComment
+=======
+>>>>>>> b7a9043 (Impliment create comment mutation)
     field :create_user, mutation: Mutations::Users::CreateUser
+    field :create_comment, mutation: Mutations::Comments::CreateComment
   end
 end
