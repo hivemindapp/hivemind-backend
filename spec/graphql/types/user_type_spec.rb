1c4d300402842263c2a99ddb642bf5c947e70e47
Richard <rdesilvey@gmail.com>
2021-08-29 10:36:30 -0600

    remove dup

--- spec/graphql/types/user_type_spec.rb
+++ spec/graphql/types/user_type_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Types::UserType do
 
   it 'has the expected fields available' do
     expect(subject).to have_field(:id)
-    expect(subject).to have_field(:id)
     expect(subject).to have_field(:username).of_type(!types.String)
     expect(subject).to have_field(:region).of_type(!types.String)
     expect(subject).to have_field(:biography).of_type(types.String)
