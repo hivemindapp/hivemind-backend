6e591463cfecbfd2559200bf7559c7331d2ece4c
Richard <rdesilvey@gmail.com>
2021-08-29 10:36:50 -0600

    Add mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_post, mutation: Mutations::Posts::CreatePost
+    
+    field :create_user, mutation: Mutations::Users::CreateUser
   end
 end
