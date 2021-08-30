465c1498d06bbd84fa71d1c5aeb79f7bfcf3f88e
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-30 17:56:43 -0600

    Reset cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000', 'hivemindapp.netlify.app'
 
     resource '*',
       headers: :any,
