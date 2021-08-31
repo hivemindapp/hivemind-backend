d198785105ace55f87363f5f04499a240a51796f
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-30 18:20:22 -0600

    Remove pry

--- spec/graphql/types/post_type_spec.rb
+++ spec/graphql/types/post_type_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe Types::PostType do
     expect(subject).to have_field(:upvotes).of_type(types.Int)
     expect(subject).to have_field(:downvotes).of_type(types.Int)
     expect(subject).to have_field(:user_id).of_type(!types.Int)
-    # require 'pry'; binding.pry
     expect(subject).to have_field(:createdAt).of_type('ISO8601DateTime!')
     expect(subject).to have_field(:updatedAt).of_type('ISO8601DateTime!')
   end
