81a873c8f218b6dc788a69caf162bc99f9440a06
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-31 13:50:17 -0600

    Add testing for ActiveStorage relationships

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Post, type: :model do
   describe 'relationships' do
     it { should belong_to :user }
     it { should have_many(:comments).dependent(:destroy) }
+    it { should have_many_attached :images}
   end
 end
