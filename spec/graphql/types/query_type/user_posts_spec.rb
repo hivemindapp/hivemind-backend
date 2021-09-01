8ba71da6f6a10127f47ce4b5821004a56059ced8
Molly Krumholz <mkkrumholz@gmail.com>
2021-09-01 17:11:35 -0600

    Update tests for user_posts query

--- spec/graphql/types/query_type/user_posts_spec.rb
+++ spec/graphql/types/query_type/user_posts_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe Types::QueryType, type: :request do
     types = GraphQL::Define::TypeDefiner.instance
 
     it 'lists all posts by the given user' do
-      user1 = create(:user)
-      user2 = create(:user)
+      user1 = create(:user, :with_avatar)
+      user2 = create(:user, :with_avatar)
 
-      posts1 = create_list(:post, 7, user: user1)
+      posts1 = create_list(:post, 3, :with_images, user: user1)
+      posts1a = create_list(:post, 2, :with_one_image, user: user1)
+      posts1b = create_list(:post, 2, user: user1)
       posts2 = create_list(:post, 5, user: user2)
 
       post '/graphql', params: { query: query(user_id: user1.id) }
@@ -21,13 +23,15 @@ RSpec.describe Types::QueryType, type: :request do
       expect(data).to be_an Array
       expect(data.length).to eq 7
 
+      user = { id: user1.id.to_s, username: user1.username, avatar: rails_blob_url(user1.avatar, only_path: true) }
+
       expect(data.first[:id]).to eq user1.posts.first.id.to_s
       expect(data.first[:title]).to be_a String
-      expect(data.first[:image]).to be_a String
+      expect(data.first[:imageUrls]).to be_an Array
       expect(data.first[:upvotes]).to be_a Integer
       expect(data.first[:downvotes]).to be_a Integer
       expect(data.first[:createdAt]).to be_a String
-      expect(data.first[:user]).to eq({ id: user1.id.to_s, username: user1.username, avatar: user1.avatar })
+      expect(data.first[:user]).to eq(user)
     end
 
     it 'returns post type objects' do
@@ -40,7 +44,7 @@ RSpec.describe Types::QueryType, type: :request do
           userPosts(userId: #{user_id}) {
             id
             title
-            image
+            imageUrls
             upvotes
             downvotes
             createdAt
