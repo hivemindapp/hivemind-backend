1154878fe973551a73bf7cedc3b823b03c52b361
Molly Krumholz <mkkrumholz@gmail.com>
2021-09-01 17:08:18 -0600

    Update tests for posts query

--- spec/graphql/types/query_type/posts_spec.rb
+++ spec/graphql/types/query_type/posts_spec.rb
@@ -7,12 +7,13 @@ RSpec.describe Types::QueryType, type: :request do
     types = GraphQL::Define::TypeDefiner.instance
 
     it 'lists all posts ordered by created date (newest to oldest)' do
-      user1 = create(:user)
-      user2 = create(:user)
+      user1 = create(:user, :with_avatar)
+      user2 = create(:user, :with_avatar)
 
-      posts1 = create_list(:post, 5, user: user1)
-      posts2 = create_list(:post, 5, user: user2)
-
+      posts1 = create_list(:post, 5, :with_images, user: user1)
+      posts1a = create_list(:post, 1, user: user2)
+      posts2 = create_list(:post, 4, :with_one_image, user: user2)
+      
       post '/graphql', params: { query: query }
 
       json = JSON.parse(response.body, symbolize_names: true)
@@ -21,13 +22,19 @@ RSpec.describe Types::QueryType, type: :request do
       expect(data).to be_an Array
       expect(data.length).to eq 10
       
+      images = posts2.last.images.map do |image|
+        rails_blob_url(image, only_path: true)
+      end
+
+      user = { id: user2.id.to_s, username: user2.username, avatar: rails_blob_url(user2.avatar, only_path: true) }
+
       expect(data.first[:id]).to eq posts2.last.id.to_s
       expect(data.first[:title]).to eq posts2.last.title
-      expect(data.first[:image]).to eq posts2.last.image
+      expect(data.first[:imageUrls]).to eq images
       expect(data.first[:upvotes]).to eq posts2.last.upvotes
       expect(data.first[:downvotes]).to eq posts2.last.downvotes
       expect(data.first[:createdAt]).to be_a String
-      expect(data.first[:user]).to eq({ id: user2.id.to_s, username: user2.username, avatar: user2.avatar })
+      expect(data.first[:user]).to eq(user)
     end
 
     it 'returns post type objects' do
@@ -40,7 +47,7 @@ RSpec.describe Types::QueryType, type: :request do
           posts {
             id
             title
-            image
+            imageUrls
             upvotes
             downvotes
             createdAt
