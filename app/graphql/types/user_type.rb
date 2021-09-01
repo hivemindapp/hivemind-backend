d90f84ae63de31f69f0023d2de88ae3928e35247
Richard <rdesilvey@gmail.com>
2021-09-01 11:34:10 -0600

    allow null region

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,7 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :username, String, null: false
-    field :region, String, null: false
+    field :region, String, null: true
     field :biography, String, null: true
     field :avatar, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
