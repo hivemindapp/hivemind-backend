4bd7daa84d0365df7e66aedff37c77df92463ded
Richard <rdesilvey@gmail.com>
2021-09-01 11:24:48 -0600

    Fix conflict

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,11 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::Users::CreateUser
     field :create_post, mutation: Mutations::Posts::CreatePost
-<<<<<<< HEAD
     field :create_comment, mutation: Mutations::Comments::CreateComment
-=======
-    
-    field :create_user, mutation: Mutations::Users::CreateUser
->>>>>>> create-user-endpoint
   end
 end
