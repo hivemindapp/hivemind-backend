560bdfc51ba089007c9fce3e7e1bd5504a2762aa
Molly Krumholz <mkkrumholz@gmail.com>
2021-09-01 15:56:06 -0600

    Update PostType for image_urls array

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :description, String, null: false
-    field :image, String, null: true
+    field :image_urls, [String], null: true
     field :upvotes, Integer, null: true
     field :downvotes, Integer, null: true
     field :user_id, Integer, null: false
@@ -11,5 +11,12 @@ module Types
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :user, Types::UserType, null: false
     field :comments, [Types::CommentType], null: true
+
+    def image_urls
+      post.images.map do |image|
+        require 'pry'; binding.pry
+        Rails.application.routes.url_helpers.rails_blob_url(image)
+      end
+    end
   end
 end
