99b8db504c5b38e5ececebc15cf30333b9693147
Molly Krumholz <mkkrumholz@gmail.com>
2021-09-01 22:38:17 -0600

    Remove rebase remnants

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_post, mutation: Mutations::Posts::CreatePost
-<<<<<<< HEAD
-    field :create_comment, mutation: Mutations::Comments::CreateComment
-=======
->>>>>>> b7a9043 (Impliment create comment mutation)
     field :create_user, mutation: Mutations::Users::CreateUser
     field :create_comment, mutation: Mutations::Comments::CreateComment
     field :create_direct_upload, mutation: Mutations::Blobs::CreateDirectUpload
